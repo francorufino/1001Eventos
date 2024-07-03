99516e6ba5e20ad7b069ca0395b5f20106b5bc2e
Andreza <campbell.andreza@gmail.com>
2024-07-03 16:31:54 -0400

    fix herosection margin smaller screens rotation words

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -23,22 +23,22 @@ const HeroSection = () => {
         <span className="bg-mblack opacity-60 w-full h-full object-cover"></span>
       </div>
       <section
-        className={`relative lg:pt-48 z-10 flex flex-col items-center font-bold justify-center h-full text-white ${gfrib.className}`}
+        className={`relative z-10 flex flex-col items-center font-bold justify-center h-full text-white ${gfrib.className}`}
       >
-        <p className="text-4xl md:text-8xl pt-32 md:pt-0  px-6 xl:pt-0 lg:text-8xl text-center font-bold flex flex-wrap text-myellow">
+        <p className="text-4xl md:text-8xl  lg:text-8xl text-center font-bold flex flex-wrap text-myellow">
           1001 EVENTOS
         </p>
         <section className="relative h-32 w-full flex justify-center items-center">
-          <span className="text-4xl md:text-6xl lg:text-7xl -mt-16 text-center font-bold text-mwhite myRotatingWords">
+          <span className="text-4xl md:-mt-16 md:text-6xl lg:text-7xl text-center font-bold text-mwhite myRotatingWords">
             Shows
           </span>
-          <span className="text-4xl md:text-6xl lg:text-7xl -mt-16 text-center font-bold text-mwhite myRotatingWords">
+          <span className="text-4xl md:-mt-16 md:text-6xl lg:text-7xl text-center font-bold text-mwhite myRotatingWords">
             Palestras
           </span>
-          <span className="text-4xl md:text-6xl lg:text-7xl -mt-8  text-center font-bold text-mwhite myRotatingWords">
+          <span className="text-4xl md:-mt-16 md:text-6xl lg:text-7xl  text-center font-bold text-mwhite myRotatingWords">
             Eventos Coorporativos
           </span>
-          <span className="text-4xl md:text-6xl lg:text-7xl -mt-16 text-center font-bold text-mwhite myRotatingWords">
+          <span className="text-4xl md:-mt-16 md:text-6xl lg:text-7xl  text-center font-bold text-mwhite myRotatingWords">
             Exposições
           </span>
         </section>
